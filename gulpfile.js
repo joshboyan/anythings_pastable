158b0daca01df76e0de58ec68c9dbffcffde8ca1
joshboyan <joshboyan@yahoo.com>
2016-10-25 13:47:31 -0700

    Add npm and gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var browserSync = require('browser-sync').create();
+ 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+             //Change address below to load different page with browserSync
+            index: "menu.html"
+        }
+    });
+    gulp.watch("styles/*.less", ['less']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('less', function () {
+  return gulp.src('./styles/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['browser-sync']);
